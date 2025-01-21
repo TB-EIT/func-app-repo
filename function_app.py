19fde27334ec1f239e7717dc7013691dda3b8eb2
Taras Basiuk <BasiukTV@gmail.com>
2025-01-21 10:15:11 -0500

    Adding a Blob trigger function scafolding.

--- function_app.py
+++ function_app.py
@@ -23,3 +23,9 @@ def http_trigger_app1(req: func.HttpRequest) -> func.HttpResponse:
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
         )
+
+@app.blob_trigger(arg_name="myblob", path="blob-trigger-app2", connection="tarasclptestsa_STORAGE")
+def BlobTriggerApp2(myblob: func.InputStream):
+    logging.info(f"Python blob trigger function processed blob"
+                f"Name: {myblob.name}"
+                f"Blob Size: {myblob.length} bytes")
